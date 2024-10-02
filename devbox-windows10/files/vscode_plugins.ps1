6aabd849d69597e75d15d636e9d9ca39b55826a5
Samir Benzammour <samir.benzammour@gmail.com>
2024-10-02 08:25:29 +0200

    feat(devbox-w10): add vscode extensions

---
+++ devbox-windows10/files/vscode_plugins.ps1
@@ -0,0 +1,16 @@
+RefreshEnvPath
+
+# PowerShell
+code --install-extension ms-vscode.PowerShell
+
+# C/C++
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.cpptools-themes
+code --install-extension twxs.cmake
+code --install-extension ms-vscode.cmake-tools
+code --install-extension SonarSource.sonarlint-vscode
+code --install-extension ms-dotnettools.csharp
+code --install-extension ms-dotnettools.csdevkit
+code --install-extension ms-vscode.mono-debug
+
+
