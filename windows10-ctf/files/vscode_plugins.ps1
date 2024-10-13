08307eedae04e0d764fa2673d56cf206c8e76b73
Samir Benzammour <samir.benzammour@gmail.com>
2024-10-13 22:39:12 +0200

    feat(w10-ctf): initial setup

---
+++ windows10-ctf/files/vscode_plugins.ps1
@@ -0,0 +1,14 @@
+RefreshEnvPath
+
+# PowerShell
+code --install-extension ms-vscode.PowerShell
+
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.cpptools-themes
+code --install-extension twxs.cmake
+code --install-extension ms-vscode.cmake-tools
+code --install-extension ms-dotnettools.csharp
+code --install-extension ms-dotnettools.csdevkit
+code --install-extension ms-vscode.mono-debug
+
+
